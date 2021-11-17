279f8005a2eef973f5be605da789aacb1b82f74a
AngeloNascimento <81114877+AngeloNascimento@users.noreply.github.com>
2021-11-16 21:30:43 -0300

    Add files via upload

--- funcoes_ex3.py
+++ funcoes_ex3.py
@@ -1,11 +1,31 @@
-lado1 = int(input("Digite lado 1: "))
-lado2 = int(input("Digite lado 2: "))
-base = int(input("Digite base: "))
-def triangulo(lado1, lado2, base):
-    if lado1 == lado2 and lado1 != base:
+def triangulo_equilatero(lado1, lado2, lado3):
+    if lado1 == lado2 and lado1 == lado3 :
         return True
     else:
         return False
 
-resultado = triangulo(lado1, lado2, base)
-print(resultado)
+def triangulo_isocele(lado1, lado2, lado3):
+    if lado1 == lado2 and lado1 != lado3 :
+        return True
+    else:
+        return False
+
+def triangulo_escaleno(lado1, lado2, lado3):
+    if lado1 != lado2 and lado1 !=lado3 and lado2 != lado3:
+        return True
+    else: 
+        return False
+
+
+
+lado1 = int(input("Digite lado 1 : "))
+lado2 = int(input("Digite lado 2 : "))
+lado3 = int(input("Digite lado 3 : "))
+
+
+resultado_isoceles = triangulo_isocele(lado1, lado2, lado3)
+print(f'triangulo isoceles = {resultado_isoceles}')
+resultado_escaleno = triangulo_escaleno(lado1, lado2, lado3)
+print(f'triangulo escaleno = {resultado_escaleno}')
+resultado_equilatero = triangulo_equilatero(lado1, lado2, lado3)
+print(f'triangulo equilatero = {resultado_equilatero}')
