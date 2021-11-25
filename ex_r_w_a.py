480f6817134ccad3350dec8221979997e12ca42c
AngeloNascimento <81114877+AngeloNascimento@users.noreply.github.com>
2021-11-25 19:19:56 -0300

    Update ex_r_w_a.py

--- ex_r_w_a.py
+++ ex_r_w_a.py
@@ -3,4 +3,4 @@ texto = input("insira alguma coisa")
 arq.write(texto)
 arq.close()
 
-arq2 = open("teste/wirite2.txt","w")
+arq2 = open("teste/wirite2.txt","w") #adicionar elementos um atras do outro
